076e58e07d32f3079d5fd9e91541125d38bd3b03
Mina Andjelkovic <andjelkovicmina@gmail.com>
2020-10-18 00:26:44 -0700

    - Created the user model and the sign-in route

--- mobile/App/util/api.js
+++ mobile/App/util/api.js
@@ -1,4 +1,6 @@
-const BASE_URL = "http://localhost:3000";
+//const BASE_URL = "http://localhost:3000";
+const BASE_URL = "http://192.168.86.44:3000";
+
 
 export const reviewApi = (path, options = {}) =>
   fetch(`${BASE_URL}/api${path}`, options).then(async res => {
