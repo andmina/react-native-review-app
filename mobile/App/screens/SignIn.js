cc63e3dc29f6537cb2642a1a157747eee6cc84be
Mina Andjelkovic <andjelkovicmina@gmail.com>
2020-10-18 13:50:36 -0700

    - Implemented Sign In from app but user not making an authentiucated request yet

--- mobile/App/screens/SignIn.js
+++ mobile/App/screens/SignIn.js
@@ -1,19 +1,38 @@
-import React from "react";
-import { ScrollView } from "react-native";
+import React from 'react';
+import { ScrollView } from 'react-native';
 
-import { TextField, ErrorText } from "../components/Form";
-import { Button } from "../components/Button";
+import { TextField, ErrorText } from '../components/Form';
+import { Button } from '../components/Button';
+import { reviewApi, saveAuthToken } from '../util/api';
 
 export default class SignIn extends React.Component {
   state = {
-    email: "",
-    password: "",
-    error: ""
+    email: '',
+    password: '',
+    error: '',
   };
 
+
   handleSubmit = () => {
-    this.setState({ error: "" });
-    this.props.navigation.navigate("Information");
+    this.setState({ error: '' });
+    // make a request to our API
+    reviewApi('/sign-in', { 
+      method: 'POST', // this is what we're expecting
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+      }),
+    })
+      // get a response back
+      .then(response => {
+        return saveAuthToken(response.result.token);   // save token in async storage
+      })
+      .then(() => {
+        this.props.navigation.navigate('Information'); // send the user to the "Information" screen
+      })
+      .catch(error => {
+        this.setState({ error: error.message });
+      });
   };
 
   render() {
--- mobile/App/util/api.js
+++ mobile/App/util/api.js
@@ -1,9 +1,27 @@
-//const BASE_URL = "http://localhost:3000";
+import { AsyncStorage } from 'react-native';
+
+// const BASE_URL = "http://localhost:3000";
 const BASE_URL = "http://192.168.86.44:3000";
+const AUTH_TOKEN = 'ReviewApp::AUTH_TOKEN';
 
+export const saveAuthToken = token => {
+  if (!token) { // if there's not a token
+    return AsyncStorage.removeItem(AUTH_TOKEN); // once someone presses Sign Out we're not saving the token anymore
+  }
 
-export const reviewApi = (path, options = {}) =>
-  fetch(`${BASE_URL}/api${path}`, options).then(async res => {
+  return AsyncStorage.setItem(AUTH_TOKEN, token); // set item AUTH_TOKEN to be the token
+};
+
+export const reviewApi = (path, options = {}) => {
+  const completeOptions = {
+    ...options, // copy all of our options
+    headers: {
+      ...options.headers,
+      'Content-Type': 'application/json', // so it exppects us to be sending json
+    },
+  };
+
+  return fetch(`${BASE_URL}/api${path}`, completeOptions).then(async res => {
     const responseJson = await res.json();
 
     if (res.ok) {
@@ -12,3 +30,4 @@ export const reviewApi = (path, options = {}) =>
 
     throw new Error(responseJson.error);
   });
+};
